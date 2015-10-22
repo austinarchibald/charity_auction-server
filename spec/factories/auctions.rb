b510c109d176c282cc7bddde73e87cc53c05462a
Austin Archibald <austin.archibald@gmail.com>
2015-10-22 16:00:33 -0400

    Added Auction model

---
+++ spec/factories/auctions.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :auction do
+    starts_at "2015-10-22 14:10:46"
+ends_at "2015-10-22 14:10:46"
+time_zone_id "MyString"
+physical_address "MyText"
+name "MyString"
+donation_window_ends_at "2015-10-22 14:10:46"
+  end
+
+end
